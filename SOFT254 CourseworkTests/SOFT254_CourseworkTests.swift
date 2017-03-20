55d4cd4ad51fee773622ef9c619eea0353fa7af3
(s) Christopher Dyson <cdyson@pm7-0671.local>
2017-03-20 12:12:49 +0000

    Initial Commit

---
+++ SOFT254 CourseworkTests/SOFT254_CourseworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  SOFT254_CourseworkTests.swift
+//  SOFT254 CourseworkTests
+//
+//  Created by (s) Christopher Dyson on 20/03/2017.
+//  Copyright © 2017 (s) Christopher Dyson. All rights reserved.
+//
+
+import XCTest
+@testable import SOFT254_Coursework
+
+class SOFT254_CourseworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
