538f62147d054d305b6a0aeeabab54b5734a776e
Chris <chris@Chriss-MacBook-Pro.local>
2017-04-10 16:08:21 +0100

    lecturer page, gets array of students in sql db from php and inserts to tableview. timer counts, shows numbers at this point. stop session stops the counter

--- SOFT254 Coursework/ViewControllerLecturer.swift
+++ SOFT254 Coursework/ViewControllerLecturer.swift
@@ -8,28 +8,91 @@
 
 import UIKit
 
-class ViewControllerLecturer: UIViewController {
+class ViewControllerLecturer: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadJSON()
+        scheduledTimerWithTimeInterval()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func btnMarkComplete(_ sender: Any) {
+    }
+    @IBAction func btnStopSession(_ sender: Any) {
+        timer.invalidate()
+    }
+    @IBOutlet weak var lblLecTitle: UILabel!
+    var myArray : [String] = []
+    var timer = Timer()
+    
+    func scheduledTimerWithTimeInterval(){
+        // Scheduling timer to Call the function **Countdown** with the interval of 1 seconds
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateCounting), userInfo: nil, repeats: true)
+        //timer.invalidate   to stop timer
+    }
+    var i = 0
+    func updateCounting(){
+        NSLog("counting..")
+        print("stuff")
+        i = i + 1
+        lblLecTitle.text = String(i)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func loadJSON() {
+        let url = URL(string: "http://46.32.240.33/ios.cdysonplym.co.uk/admin.php")
+        URLSession.shared.dataTask(with:url!, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else { return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                
+                var query = json["0"]
+                if let q = query {
+                    self.myArray.append(String(describing:q))
+                }
+                query = json["1"]
+                if let q = query {
+                    self.myArray.append(String(describing:q))
+                }
+                query = json["2"]
+                if let q = query {
+                    self.myArray.append(String(describing:q))
+                }
+                query = json["3"]
+                if let q = query {
+                    self.myArray.append(String(describing:q))
+                }
+                DispatchQueue.main.async{
+                    self.tableView.reloadData()
+                }
+                
+            } catch let error as NSError {
+                print(error)
+            }
+        }).resume()
     }
-    */
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel?.text = self.myArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedString = self.myArray[indexPath.row]
+        print("You picked \(selectedString)")
+    }
+    
 }
